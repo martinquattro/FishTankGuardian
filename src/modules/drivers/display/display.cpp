cb83d3e57479510c59f17ca784b1565fbe97f614
martinquattro <mquattrone@fi.uba.ar>
2023-11-20 02:52:34 +0000

    DisplayInit

--- src/modules/drivers/display/display.cpp
+++ src/modules/drivers/display/display.cpp
@@ -1,38 +1,384 @@
-/*!****************************************************************************
- * @file display.cpp
- * @brief TODO
- * @author Quattrone Martin
- * @date Oct 2023
- *******************************************************************************/
-
 //=====[Libraries]=============================================================
 
-#include "mbed.h"
 #include "display.h"
 
-namespace Drivers {
+#include "arm_book_lib.h"
+#include "mbed.h"
+
+//=====[Declaration and initialization of public global objects]===============
+
+DigitalOut displayD0( D0 );
+DigitalOut displayD1( D1 );
+DigitalOut displayD2( D2 );
+DigitalOut displayD3( D3 );
+DigitalOut displayD4( D4 );
+DigitalOut displayD5( D5 );
+DigitalOut displayD6( D6 );
+DigitalOut displayD7( D7 );
+DigitalOut displayRs( D8 );
+DigitalOut displayEn( D9 );
+
+I2C i2cPcf8574( I2C1_SDA, I2C1_SCL ); 
+
+DigitalOut spiSt7920ChipSelect(SPI1_CS);
+SPI spiSt7920(SPI1_MOSI, SPI1_MISO, SPI1_SCK);
 
 //=====[Declaration and initialization of private global variables]============
 
-Display* Display::mInstance = nullptr;
+static display_t display;
+static pcf8574_t pcf8574;
+static bool initial8BitCommunicationIsCompleted;
+
+//=====[Declarations (prototypes) of private functions]========================
+
+static void displayPinWrite( int pinName, int value );
+static void displayDataBusWrite( int dataByte );
+static void displayCodeWrite( bool type, int dataBus );
 
 //=====[Implementations of public functions]===================================
 
-//----static-------------------------------------------------------------------
-void Display::Init(int pin)
+//-----------------------------------------------------------------------------
+void displayInit(displayType_t type, displayConnection_t connection)
 {
-    if (mInstance == nullptr)
+    DEBUG_PRINT("displayInit\r\n");
+    
+    display.type = type;
+    display.connection = connection;
+    display.mode = DISPLAY_MODE_CHAR;
+    
+    initial8BitCommunicationIsCompleted = false;    
+
+    delay( 50 );
+    
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_FUNCTION_SET | 
+                      DISPLAY_IR_FUNCTION_SET_8BITS );
+    delay( 5 );
+            
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_FUNCTION_SET | 
+                      DISPLAY_IR_FUNCTION_SET_8BITS );
+    delay( 1 ); 
+
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_FUNCTION_SET | 
+                      DISPLAY_IR_FUNCTION_SET_8BITS );
+    delay( 1 );  
+
+    switch( display.connection ) {
+        case DISPLAY_CONNECTION_GPIO_8BITS:
+        case DISPLAY_CONNECTION_SPI:
+            displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                              DISPLAY_IR_FUNCTION_SET | 
+                              DISPLAY_IR_FUNCTION_SET_8BITS | 
+                              DISPLAY_IR_FUNCTION_SET_2LINES |
+                              DISPLAY_IR_FUNCTION_SET_5x8DOTS );
+            delay( 1 );         
+        break;
+        
+        case DISPLAY_CONNECTION_GPIO_4BITS:
+        case DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER:
+            displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                              DISPLAY_IR_FUNCTION_SET | 
+                              DISPLAY_IR_FUNCTION_SET_4BITS );
+            delay( 1 );  
+
+            initial8BitCommunicationIsCompleted = true;  
+
+            displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                              DISPLAY_IR_FUNCTION_SET | 
+                              DISPLAY_IR_FUNCTION_SET_4BITS | 
+                              DISPLAY_IR_FUNCTION_SET_2LINES |
+                              DISPLAY_IR_FUNCTION_SET_5x8DOTS );
+            delay( 1 );                                      
+        break;
+    }
+
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_DISPLAY_CONTROL |
+                      DISPLAY_IR_DISPLAY_CONTROL_DISPLAY_OFF |      
+                      DISPLAY_IR_DISPLAY_CONTROL_CURSOR_OFF |       
+                      DISPLAY_IR_DISPLAY_CONTROL_BLINK_OFF );       
+    delay( 1 );          
+
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_CLEAR_DISPLAY );       
+    delay( 1 ); 
+
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_ENTRY_MODE_SET |
+                      DISPLAY_IR_ENTRY_MODE_SET_INCREMENT |       
+                      DISPLAY_IR_ENTRY_MODE_SET_NO_SHIFT );                  
+    delay( 1 );           
+
+    displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                      DISPLAY_IR_DISPLAY_CONTROL |
+                      DISPLAY_IR_DISPLAY_CONTROL_DISPLAY_ON |      
+                      DISPLAY_IR_DISPLAY_CONTROL_CURSOR_OFF |    
+                      DISPLAY_IR_DISPLAY_CONTROL_BLINK_OFF );    
+    delay( 1 );  
+}
+
+//-----------------------------------------------------------------------------
+void displayCharPositionWrite( int charPositionX, int charPositionY )
+{    
+    if( display.type == DISPLAY_TYPE_LCD_HD44780 )
     {
-        mInstance = new Display(pin);
+        switch( charPositionY ) {
+            case 0:
+                displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                                  DISPLAY_IR_SET_DDRAM_ADDR |
+                                  ( DISPLAY_20x4_LINE1_FIRST_CHARACTER_ADDRESS +
+                                    charPositionX ) );
+                delay( 1 );         
+            break;
+        
+            case 1:
+                displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                                  DISPLAY_IR_SET_DDRAM_ADDR |
+                                  ( DISPLAY_20x4_LINE2_FIRST_CHARACTER_ADDRESS +
+                                    charPositionX ) );
+                delay( 1 );         
+            break;
+        
+            case 2:
+                displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                                  DISPLAY_IR_SET_DDRAM_ADDR |
+                                  ( DISPLAY_20x4_LINE3_FIRST_CHARACTER_ADDRESS +
+                                    charPositionX ) );
+                delay( 1 );         
+            break;
+
+            case 3:
+                displayCodeWrite( DISPLAY_RS_INSTRUCTION, 
+                                  DISPLAY_IR_SET_DDRAM_ADDR |
+                                  ( DISPLAY_20x4_LINE4_FIRST_CHARACTER_ADDRESS +
+                                    charPositionX ) );
+                delay( 1 );         
+            break;
+         }
     }
 }
 
-//----static-------------------------------------------------------------------
-Display* Display::GetInstance()
+//-----------------------------------------------------------------------------
+void displayStringWrite( const char * str )
 {
-    return mInstance;
+    while (*str) 
+    {
+        displayCodeWrite(DISPLAY_RS_DATA, *str++);
+    }
+}
+
+//-----------------------------------------------------------------------------
+void displayClear( void )
+{
+    displayCodeWrite(DISPLAY_RS_INSTRUCTION, DISPLAY_IR_CLEAR_DISPLAY);
+    delay(2); 
 }
 
 //=====[Implementations of private functions]==================================
 
-} // namespace Drivers
+//----static-------------------------------------------------------------------
+static void displayCodeWrite(bool type, int dataBus)
+{
+    switch(display.connection) 
+    {
+        case DISPLAY_CONNECTION_GPIO_8BITS:
+        case DISPLAY_CONNECTION_GPIO_4BITS:
+        case DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER:    
+            if (type == DISPLAY_RS_INSTRUCTION)
+                displayPinWrite( DISPLAY_PIN_RS, DISPLAY_RS_INSTRUCTION);
+                else
+                displayPinWrite( DISPLAY_PIN_RS, DISPLAY_RS_DATA);
+            displayPinWrite( DISPLAY_PIN_RW, DISPLAY_RW_WRITE );
+            displayDataBusWrite( dataBus );
+        break;
+  
+        case DISPLAY_CONNECTION_SPI:
+            spiSt7920.lock();
+            spiSt7920ChipSelect = ON;
+            if ( type == DISPLAY_RS_INSTRUCTION )           
+                spiSt7920.write( ST7920_SPI_SYNCHRONIZING_BIT_STRING |
+                                  ST7920_SPI_RW_WRITE |
+                                  ST7920_SPI_RS_INSTRUCTION );                              
+                else
+                spiSt7920.write( ST7920_SPI_SYNCHRONIZING_BIT_STRING |
+                                  ST7920_SPI_RW_WRITE |
+                                  ST7920_SPI_RS_DATA );               
+            spiSt7920.write( dataBus & 0b11110000 );      
+            spiSt7920.write( (dataBus<<4) & 0b11110000 );
+            spiSt7920ChipSelect = OFF;
+            spiSt7920.unlock();
+        break;
+    }    
+}
+
+//----static-------------------------------------------------------------------
+static void displayPinWrite( int pinName, int value )
+{
+    switch( display.connection ) {
+        case DISPLAY_CONNECTION_GPIO_8BITS:
+            switch( pinName ) {
+                case DISPLAY_PIN_D0: displayD0 = value;   break;
+                case DISPLAY_PIN_D1: displayD1 = value;   break;
+                case DISPLAY_PIN_D2: displayD2 = value;   break;
+                case DISPLAY_PIN_D3: displayD3 = value;   break;
+                case DISPLAY_PIN_D4: displayD4 = value;   break;
+                case DISPLAY_PIN_D5: displayD5 = value;   break;
+                case DISPLAY_PIN_D6: displayD6 = value;   break;
+                case DISPLAY_PIN_D7: displayD7 = value;   break;
+                case DISPLAY_PIN_RS: displayRs = value;   break;
+                case DISPLAY_PIN_EN: displayEn = value;   break;
+                case DISPLAY_PIN_RW: break; 
+                default: break;
+            }
+            break;
+        case DISPLAY_CONNECTION_GPIO_4BITS:
+            switch( pinName ) {
+                case DISPLAY_PIN_D4: displayD4 = value;   break;
+                case DISPLAY_PIN_D5: displayD5 = value;   break;
+                case DISPLAY_PIN_D6: displayD6 = value;   break;
+                case DISPLAY_PIN_D7: displayD7 = value;   break;
+                case DISPLAY_PIN_RS: displayRs = value;   break;
+                case DISPLAY_PIN_EN: displayEn = value;   break;
+                case DISPLAY_PIN_RW: break; 
+                default: break;
+            }
+            break;
+        case DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER:
+           if ( value ) {
+                switch( pinName ) {
+                    case DISPLAY_PIN_D4: pcf8574.displayPinD4 = ON; break;
+                    case DISPLAY_PIN_D5: pcf8574.displayPinD5 = ON; break;
+                    case DISPLAY_PIN_D6: pcf8574.displayPinD6 = ON; break;
+                    case DISPLAY_PIN_D7: pcf8574.displayPinD7 = ON; break;
+                    case DISPLAY_PIN_RS: pcf8574.displayPinRs = ON; break;
+                    case DISPLAY_PIN_EN: pcf8574.displayPinEn = ON; break;
+                    case DISPLAY_PIN_RW: pcf8574.displayPinRw = ON; break;
+                    case DISPLAY_PIN_A_PCF8574: pcf8574.displayPinA = ON; break;
+                    default: break;
+                }
+            }
+            else {
+                switch( pinName ) {
+                    case DISPLAY_PIN_D4: pcf8574.displayPinD4 = OFF; break;
+                    case DISPLAY_PIN_D5: pcf8574.displayPinD5 = OFF; break;
+                    case DISPLAY_PIN_D6: pcf8574.displayPinD6 = OFF; break;
+                    case DISPLAY_PIN_D7: pcf8574.displayPinD7 = OFF; break;
+                    case DISPLAY_PIN_RS: pcf8574.displayPinRs = OFF; break;
+                    case DISPLAY_PIN_EN: pcf8574.displayPinEn = OFF; break;
+                    case DISPLAY_PIN_RW: pcf8574.displayPinRw = OFF; break;
+                    case DISPLAY_PIN_A_PCF8574: pcf8574.displayPinA = OFF; break;
+                    default: break;
+                }
+            }     
+            pcf8574.data = 0b00000000;
+            if ( pcf8574.displayPinRs ) pcf8574.data |= 0b00000001; 
+            if ( pcf8574.displayPinRw ) pcf8574.data |= 0b00000010; 
+            if ( pcf8574.displayPinEn ) pcf8574.data |= 0b00000100; 
+            if ( pcf8574.displayPinA  ) pcf8574.data |= 0b00001000; 
+            if ( pcf8574.displayPinD4 ) pcf8574.data |= 0b00010000; 
+            if ( pcf8574.displayPinD5 ) pcf8574.data |= 0b00100000; 
+            if ( pcf8574.displayPinD6 ) pcf8574.data |= 0b01000000; 
+            if ( pcf8574.displayPinD7 ) pcf8574.data |= 0b10000000; 
+            i2cPcf8574.write( pcf8574.address, &pcf8574.data, 1);
+            break;
+
+        case DISPLAY_CONNECTION_SPI:
+        break;
+    }
+}
+
+//----static-------------------------------------------------------------------
+static void displayDataBusWrite( int dataBus )
+{
+    displayPinWrite( DISPLAY_PIN_EN, OFF );
+    displayPinWrite( DISPLAY_PIN_D7, dataBus & 0b10000000 );
+    displayPinWrite( DISPLAY_PIN_D6, dataBus & 0b01000000 );
+    displayPinWrite( DISPLAY_PIN_D5, dataBus & 0b00100000 );
+    displayPinWrite( DISPLAY_PIN_D4, dataBus & 0b00010000 );
+    switch( display.connection ) {
+        case DISPLAY_CONNECTION_GPIO_8BITS:
+            displayPinWrite( DISPLAY_PIN_D3, dataBus & 0b00001000 );
+            displayPinWrite( DISPLAY_PIN_D2, dataBus & 0b00000100 );  
+            displayPinWrite( DISPLAY_PIN_D1, dataBus & 0b00000010 );      
+            displayPinWrite( DISPLAY_PIN_D0, dataBus & 0b00000001 );
+        break; 
+              
+        case DISPLAY_CONNECTION_GPIO_4BITS:
+        case DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER:
+            if ( initial8BitCommunicationIsCompleted == true) {
+                displayPinWrite( DISPLAY_PIN_EN, ON );         
+                delay( 1 );
+                displayPinWrite( DISPLAY_PIN_EN, OFF );              
+                delay( 1 );        
+                displayPinWrite( DISPLAY_PIN_D7, dataBus & 0b00001000 );
+                displayPinWrite( DISPLAY_PIN_D6, dataBus & 0b00000100 );  
+                displayPinWrite( DISPLAY_PIN_D5, dataBus & 0b00000010 );      
+                displayPinWrite( DISPLAY_PIN_D4, dataBus & 0b00000001 );                
+            }
+        break;
+        
+        case DISPLAY_CONNECTION_SPI:
+        break;
+    }
+    displayPinWrite( DISPLAY_PIN_EN, ON );              
+    delay( 1 );
+    displayPinWrite( DISPLAY_PIN_EN, OFF );  
+    delay( 1 );                   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*!****************************************************************************
+//  * @file display.cpp
+//  * @brief TODO
+//  * @author Quattrone Martin
+//  * @date Oct 2023
+//  *******************************************************************************/
+
+// //=====[Libraries]=============================================================
+
+// #include "mbed.h"
+// #include "display.h"
+
+// namespace Drivers {
+
+// //=====[Declaration and initialization of private global variables]============
+
+// Display* Display::mInstance = nullptr;
+
+// //=====[Implementations of public functions]===================================
+
+// //----static-------------------------------------------------------------------
+// void Display::Init(int pin)
+// {
+//     if (mInstance == nullptr)
+//     {
+//         mInstance = new Display(pin);
+//     }
+// }
+
+// //----static-------------------------------------------------------------------
+// Display* Display::GetInstance()
+// {
+//     return mInstance;
+// }
+
+// //=====[Implementations of private functions]==================================
+
+// } // namespace Drivers
--- src/modules/drivers/display/display.h
+++ src/modules/drivers/display/display.h
@@ -1,39 +1,188 @@
-/*!****************************************************************************
- * @file tds_sensor.h
- * @brief TODO
- * @author Quattrone Martin
- * @date Oct 2023
- *******************************************************************************/
+//=====[#include guards - begin]===============================================
 
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#ifndef _DISPLAY_H_
+#define _DISPLAY_H_
 
-namespace Drivers { 
+#define DISPLAY_IR_CLEAR_DISPLAY   0b00000001
+#define DISPLAY_IR_ENTRY_MODE_SET  0b00000100
+#define DISPLAY_IR_DISPLAY_CONTROL 0b00001000
+#define DISPLAY_IR_FUNCTION_SET    0b00100000
+#define DISPLAY_IR_SET_DDRAM_ADDR  0b10000000
 
-    class Display 
-    {
-        public:
+#define DISPLAY_IR_ENTRY_MODE_SET_INCREMENT 0b00000010
+#define DISPLAY_IR_ENTRY_MODE_SET_DECREMENT 0b00000000
+#define DISPLAY_IR_ENTRY_MODE_SET_SHIFT     0b00000001
+#define DISPLAY_IR_ENTRY_MODE_SET_NO_SHIFT  0b00000000
 
-            //! Initialize the sensor
-            static void Init(int pin);
+#define DISPLAY_IR_DISPLAY_CONTROL_DISPLAY_ON  0b00000100
+#define DISPLAY_IR_DISPLAY_CONTROL_DISPLAY_OFF 0b00000000
+#define DISPLAY_IR_DISPLAY_CONTROL_CURSOR_ON   0b00000010
+#define DISPLAY_IR_DISPLAY_CONTROL_CURSOR_OFF  0b00000000
+#define DISPLAY_IR_DISPLAY_CONTROL_BLINK_ON    0b00000001
+#define DISPLAY_IR_DISPLAY_CONTROL_BLINK_OFF   0b00000000
 
-            //! Returns the sensor object
-            static Display* GetInstance();
+#define DISPLAY_IR_FUNCTION_SET_8BITS    0b00010000
+#define DISPLAY_IR_FUNCTION_SET_4BITS    0b00000000
+#define DISPLAY_IR_FUNCTION_SET_2LINES   0b00001000
+#define DISPLAY_IR_FUNCTION_SET_1LINE    0b00000000
+#define DISPLAY_IR_FUNCTION_SET_5x10DOTS 0b00000100
+#define DISPLAY_IR_FUNCTION_SET_5x8DOTS  0b00000000
 
-            //!
-            void Read();
+#define DISPLAY_20x4_LINE1_FIRST_CHARACTER_ADDRESS 0
+#define DISPLAY_20x4_LINE2_FIRST_CHARACTER_ADDRESS 64
+#define DISPLAY_20x4_LINE3_FIRST_CHARACTER_ADDRESS 20
+#define DISPLAY_20x4_LINE4_FIRST_CHARACTER_ADDRESS 84
 
-        private:
+#define DISPLAY_RS_INSTRUCTION 0
+#define DISPLAY_RS_DATA        1
 
-            Display(int pin) : mPin(pin) {}
-            ~Display() = default;
-            Display(const Display&) = delete;
-            Display& operator=(const Display&) = delete;
+#define DISPLAY_RW_WRITE 0
+#define DISPLAY_RW_READ  1
 
-            int mPin;
-            static Display* mInstance;
-    };
+#define DISPLAY_PIN_RS  4
+#define DISPLAY_PIN_RW  5
+#define DISPLAY_PIN_EN  6
+#define DISPLAY_PIN_D0  7  
+#define DISPLAY_PIN_D1  8  
+#define DISPLAY_PIN_D2  9  
+#define DISPLAY_PIN_D3 10
+#define DISPLAY_PIN_D4 11
+#define DISPLAY_PIN_D5 12 
+#define DISPLAY_PIN_D6 13 
+#define DISPLAY_PIN_D7 14 
 
-} // namespace Drivers
+#define DISPLAY_PIN_A_PCF8574 3
 
-#endif // DISPLAY_H
+#define I2C1_SDA PB_9
+#define I2C1_SCL PB_8
+
+#define PCF8574_I2C_BUS_8BIT_WRITE_ADDRESS 78
+
+#define SPI1_MOSI PA_7
+#define SPI1_MISO PA_6
+#define SPI1_SCK  PA_5
+#define SPI1_CS   PD_14
+
+#define DISPLAY_IR_SET_GDRAM_ADDR  0b10000000
+
+#define DISPLAY_IR_FUNCTION_SET_EXTENDED_INSTRUCION_SET 0b00000100
+#define DISPLAY_IR_FUNCTION_SET_BASIC_INSTRUCION_SET    0b00000000
+#define DISPLAY_IR_FUNCTION_SET_GRAPHIC_DISPLAY_ON      0b00000010
+#define DISPLAY_IR_FUNCTION_SET_GRAPHIC_DISPLAY_OFF     0b00000000
+
+#define DISPLAY_ST7920_LINE1_FIRST_CHARACTER_ADDRESS 0
+#define DISPLAY_ST7920_LINE2_FIRST_CHARACTER_ADDRESS 16
+#define DISPLAY_ST7920_LINE3_FIRST_CHARACTER_ADDRESS 8
+#define DISPLAY_ST7920_LINE4_FIRST_CHARACTER_ADDRESS 24
+
+#define ST7920_SPI_SYNCHRONIZING_BIT_STRING 0b11111000
+
+#define ST7920_SPI_RS_INSTRUCTION 0b000000000
+#define ST7920_SPI_RS_DATA        0b000000010
+
+#define ST7920_SPI_RW_WRITE 0b000000000
+#define ST7920_SPI_RW_READ  0b000000100
+
+//=====[Declaration of private data types]=====================================
+
+typedef struct{
+    int address;
+    char data;
+    bool displayPinRs;
+    bool displayPinRw; 
+    bool displayPinEn;
+    bool displayPinA;
+    bool displayPinD4;
+    bool displayPinD5;
+    bool displayPinD6;
+    bool displayPinD7;
+} pcf8574_t;
+
+//=====[Declaration of public data types]======================================
+
+typedef enum {
+     DISPLAY_TYPE_LCD_HD44780, 
+     DISPLAY_TYPE_GLCD_ST7920,
+} displayType_t;
+
+typedef enum {
+     DISPLAY_CONNECTION_GPIO_4BITS,
+     DISPLAY_CONNECTION_GPIO_8BITS,
+     DISPLAY_CONNECTION_I2C_PCF8574_IO_EXPANDER,
+     DISPLAY_CONNECTION_SPI
+} displayConnection_t;
+
+typedef enum {
+     DISPLAY_MODE_CHAR, 
+     DISPLAY_MODE_GRAPHIC
+} displayMode_t;
+
+typedef struct {
+   displayConnection_t connection;
+   displayType_t type;                                                                   
+   displayMode_t mode;                                                  
+} display_t;
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void displayInit( displayType_t type, displayConnection_t connection );
+ 
+void displayCharPositionWrite( int charPositionX, int charPositionY );
+
+void displayStringWrite( const char * str );
+
+void displayClear( void );
+
+// GLCD display only
+
+void displayBitmapWrite( int* bitmap );
+
+//=====[#include guards - end]=================================================
+
+#endif // _DISPLAY_H_
+
+
+
+
+
+
+
+// /*!****************************************************************************
+//  * @file tds_sensor.h
+//  * @brief TODO
+//  * @author Quattrone Martin
+//  * @date Oct 2023
+//  *******************************************************************************/
+
+// #ifndef DISPLAY_H
+// #define DISPLAY_H
+
+// namespace Drivers { 
+
+//     class Display 
+//     {
+//         public:
+
+//             //! Initialize the sensor
+//             static void Init(int pin);
+
+//             //! Returns the sensor object
+//             static Display* GetInstance();
+
+//             //!
+//             void Read();
+
+//         private:
+
+//             Display(int pin) : mPin(pin) {}
+//             ~Display() = default;
+//             Display(const Display&) = delete;
+//             Display& operator=(const Display&) = delete;
+
+//             int mPin;
+//             static Display* mInstance;
+//     };
+
+// } // namespace Drivers
+
+// #endif // DISPLAY_H
